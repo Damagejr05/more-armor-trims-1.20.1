8af0bb3821b7c238687278bb7a22197cb2214b9c
Damage <ethan.camidge05@gmail.com>
2024-11-22 16:42:43 -0500

    Test

---
+++ src/main/java/net/damage/moretrims/MoreArmorTrims.java
@@ -0,0 +1,16 @@
+package net.damage.moretrims;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreArmorTrims implements ModInitializer {
+    public static final String MOD_ID = "moretrims";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/damage/moretrims/MoreArmorTrimsClient.java
@@ -0,0 +1,10 @@
+package net.damage.moretrims;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreArmorTrimsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
